739b0cb073f1f9e67be5e13c6cd0b2bb19060822
jayasimha9 <jayasimharajumudduluru@Jayasimhas-MacBook-Air.local>
2025-04-20 13:10:25 +0530

    Fix build configuration for Netlify

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,16 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: './',
   build: {
     outDir: 'dist',
-    sourcemap: true,
-    manifest: true,
+    emptyOutDir: true,
     rollupOptions: {
-      input: './index.html',
+      input: './index.html'
     }
-  },
-  server: {
-    port: 5173,
-    strictPort: true
   }
 })
